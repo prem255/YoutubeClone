c4bf7c05a01767afff43c12d0e57993c4b1af625
Prem kumar <prem.k@sarv.com>
2023-03-10 19:57:58 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { logo } from "../utils/constants";
+import SearchBar from "./SearchBar";
+
+const Navbar = () => (
+  <Stack direction="row" alignItems="center" p={2} sx={{ position: "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <div >
+      <img src={logo} alt="Youtube" height={45} /> 
+      <span style={{color:'white', font:'25px'}}>Youtube</span>
+      </div>
+    </Link>
+    <SearchBar />
+  </Stack>
+);
+
+export default Navbar;
